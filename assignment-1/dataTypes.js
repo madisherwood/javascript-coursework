18bd444d33ba4dbae345aa7689593ef7ccd96001
madisherwood <madisherwood04@gmail.com>
2026-01-28 18:25:04 -0500

    Add dataTypes.js with variable declarations and logs

---
+++ assignment-1/dataTypes.js
@@ -0,0 +1,10 @@
+let age = 19
+let fullName = "Madilynn Sherwood"
+let isEnrolled = true; 
+let futureGoal; // run a marathon
+let incompletedCourse = null; // Javascript Mode
+console.log ("Age:", age, "Type:", typeof age);
+console.log ("Full Name:", fullName, "Type:", typeof fullName);
+console.log("Enrolled:", isEnrolled, "Type:", typeof isEnrolled);
+console.log("Future Goal:", futureGoal, "Type:", typeof futureGoal);
+console.log("incompleted Course:", incompletedCourse, "Type:", typeof incompletedCourse);
