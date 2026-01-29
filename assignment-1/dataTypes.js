9abedf2c88e238af136bc87ef85a996352a3ea3d
madisherwood <madisherwood04@gmail.com>
2026-01-28 19:15:56 -0500

    Enhance dataTypes.js with type conversion examples
    Added type conversion examples for age, fullName, isEnrolled, and incompletedCourse.

--- assignment-1/dataTypes.js
+++ assignment-1/dataTypes.js
@@ -1,5 +1,5 @@
-let age = 19
-let fullName = "Madilynn Sherwood"
+let age = 19;
+let fullName = "Madilynn Sherwood";
 let isEnrolled = true; 
 let futureGoal; // run a marathon
 let incompletedCourse = null; // Javascript Mode
@@ -8,3 +8,11 @@ console.log ("Full Name:", fullName, "Type:", typeof fullName);
 console.log("Enrolled:", isEnrolled, "Type:", typeof isEnrolled);
 console.log("Future Goal:", futureGoal, "Type:", typeof futureGoal);
 console.log("incompleted Course:", incompletedCourse, "Type:", typeof incompletedCourse);
+let ageAsString = String(age);
+console.log("Original:", age, typeod age, "| Converted:", ageAsString, typeof ageAsString);
+let nameAsBoolean = Boolean(fullName);
+console.log("Original:", fullName, typeof fullName, "|Converted:", nameAsBoolean, typeof nameAsBoolean);
+let enrolledAsNumber = Number(isEnrolled);
+console.log("Original:", isEnrolled, typeof isEnrolled, "|Converted:", enrolledAsNumbe, typeof enrolledAsNumber)
+let nullAsNumber = Number(incompletedCourse);
+console.log("Original:", incompletedCourse, typeof incompletedCourse, "|Converted:", nullAsNumber, typeof nullAsNumber);
